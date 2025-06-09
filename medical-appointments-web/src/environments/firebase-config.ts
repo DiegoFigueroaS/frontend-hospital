cc9a3e62a32cb3078df0d47c331f9dccc23bfbb0
DiegoFigueroaS <diegofigueroa_dev@outlook.com>
2025-06-09 16:06:10 -0400

    base

---
+++ medical-appointments-web/src/environments/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDoaTYhR-mU4WiaStnYfz61g7sJpAcjLr4",
+  authDomain: "hospital-project-e1282.firebaseapp.com",
+  projectId: "hospital-project-e1282",
+  storageBucket: "hospital-project-e1282.appspot.com",
+  messagingSenderId: "203151242269",
+  appId: "1:203151242269:web:cee98c77cb9c2ae04302f4",
+  measurementId: "G-DHVQX7BE2F"
+};
