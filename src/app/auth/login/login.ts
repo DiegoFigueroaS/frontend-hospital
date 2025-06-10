f89e2f408ff101ba3b46ff1523cd992c331aa48c
DiegoFigueroaS <diegofigueroa_dev@outlook.com>
2025-06-10 08:23:44 -0400

    base

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,26 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-login',
   imports: [
-    RouterOutlet,
     FormsModule,
     CommonModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    RouterLink
   ],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  templateUrl: './login.html',
+  styleUrls: ['./login.scss']
 })
-export class App {
-  protected title = 'proyecto_hospital_frontend';
+export class Login {
+
 }
